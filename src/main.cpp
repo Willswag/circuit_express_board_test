7280d2cef71d589d6dac5968ef061d40e8870065
Spencer Waguespack <wswaguespack403@gmail.com>
2023-09-07 19:48:47 -0400

    playing with some LED code

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,64 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
 #define LED_LOOP_DELAY 500
 
-unsigned long last_loop_time = 0;
+#define NEOPIXEL_LOOP_DELAY 1500
+#define NEOPIXEL_COUNT 10
+#define NEOPIXEL_PIN 8
+
+void colorWipe(uint32_t c, unsigned long wait, unsigned long current_time);
+
+unsigned long last_led_loop_time = 0;
+unsigned long last_neopixel_loop_time = 0;
+
 uint8_t led_state = HIGH;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEOPIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 // put function declarations here:
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  pixels.begin();
+  pixels.clear();
+
+  pixels.show();
 }
 
-void loop() {
+void loop()
+{
   unsigned long current_time = millis();
-
-  if ((current_time - last_loop_time)> LED_LOOP_DELAY)
+  // LED test
+  if ((current_time - last_led_loop_time) > LED_LOOP_DELAY)
   {
-    digitalWrite(LED_BUILTIN,led_state);
+    digitalWrite(LED_BUILTIN, led_state);
     led_state = led_state ^ 0x01;
-    last_loop_time = current_time;
+    last_led_loop_time = current_time;
+  }
+  // neopixels
+  colorWipe(pixels.Color(0, 15, 25), 90, current_time); // Red
+}
+
+void colorWipe(uint32_t c, unsigned long wait, unsigned long current_time)
+{
+  static uint8_t i = 2;
+  static unsigned long last_loop_time = 0;
+  if ((current_time - last_loop_time) < wait)
+  {
+    return;
+  }
+  last_loop_time = current_time;
+  i++;
+  if (i > pixels.numPixels())
+  {
+    i = 2;
+  }
+  pixels.clear();
+  for (uint8_t j = i; j > (i - 2); j--)
+  {
+    pixels.setPixelColor(j, c);
   }
 
-  
+  pixels.show();
 }
